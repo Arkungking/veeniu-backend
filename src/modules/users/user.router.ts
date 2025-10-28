c79d90dcafb8a0e1097d21b7dc3a05fe2ab6ed4a
Arkungking <aryagiffari0@gmail.com>
2025-10-28 17:07:28 +0700

    update user api

---
+++ src/modules/users/user.router.ts
@@ -0,0 +1,50 @@
+import { Router } from "express";
+import { JWT_SECRET } from "../../config/env";
+import { JwtMiddleware } from "../../middlewares/jwt.middleware";
+import { UploaderMiddleware } from "../../middlewares/uploader.middleware";
+import { validateBody } from "../../middlewares/validation.middleware";
+import { UserUpdateController } from "./user.controller";
+import { UserUpdateDTO } from "./dto/user-update.dto";
+
+export class UserUpdateRouter {
+  private router: Router;
+  private userUpdateController: UserUpdateController;
+  private jwtMiddleware: JwtMiddleware;
+  private uploaderMiddleware: UploaderMiddleware;
+
+  constructor() {
+    this.router = Router();
+    this.userUpdateController = new UserUpdateController();
+    this.jwtMiddleware = new JwtMiddleware();
+    this.uploaderMiddleware = new UploaderMiddleware();
+    this.initializedRoutes();
+  }
+
+  private initializedRoutes = () => {
+    this.router.post(
+      "/",
+      this.jwtMiddleware.verifyToken(JWT_SECRET!),
+      this.jwtMiddleware.verifyRole(["CUSTOMER"]),
+      this.userUpdateController.userUpdate
+    );
+
+    this.router.patch(
+      "/user-update",
+      this.jwtMiddleware.verifyToken(JWT_SECRET!),
+      this.jwtMiddleware.verifyRole(["CUSTOMER"]),
+      this.uploaderMiddleware
+        .upload()
+        .fields([{ name: "profilePicture", maxCount: 1 }]),
+      this.uploaderMiddleware.fileFIlter([
+        "image/jpeg",
+        "image/png",
+        "image/heic",
+      ]),
+      validateBody(UserUpdateDTO),
+      this.userUpdateController.userUpdate
+    );
+  };
+  getRouter = () => {
+    return this.router;
+  };
+}
--- src/modules/users/user.service.ts
+++ src/modules/users/user.service.ts
@@ -1,11 +1,42 @@
+import { profile } from "console";
+import { ApiError } from "../../utils/api-error";
+import { CloudinaryService } from "../cloudinary/cloudinary.service";
 import { PrismaService } from "../prisma/prisma.service";
 import { UserUpdateDTO } from "./dto/user-update.dto";
 
-export class userService {
-    private prisma: PrismaService;
-    constructor() {
-        this.prisma = new PrismaService();
-    }
+export class UserUpdateService {
+  private prisma: PrismaService;
+  private cloudinaryService: CloudinaryService;
 
-    userUpdate = async (body:UserUpdateDTO) => {}
+  constructor() {
+    this.prisma = new PrismaService();
+    this.cloudinaryService = new CloudinaryService();
+  }
+
+  userUpdate = async (body: UserUpdateDTO, image?: Express.Multer.File) => {
+  const user = await this.prisma.user.findFirst({
+    where: { id: body.id },
+  });
+
+  if (!user) throw new ApiError("user not found", 404);
+
+  let imageUrl = user.profilePicture;
+
+  if (image) {
+    if (user.profilePicture) await this.cloudinaryService.remove(user.profilePicture);
+    const { secure_url } = await this.cloudinaryService.upload(image);
+    imageUrl = secure_url;
+  }
+
+  const updateData: any = {};
+  if (body.name) updateData.name = body.name;
+  updateData.image = imageUrl;
+
+  await this.prisma.user.update({
+    where: { id: body.id },
+    data: updateData,
+  });
+
+  return { message: "update user success" };
+};
 }
