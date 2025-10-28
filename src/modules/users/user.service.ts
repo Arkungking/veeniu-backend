2016a3197955e999951c70db01685bb63dad9ed0
Arkungking <aryagiffari0@gmail.com>
2025-10-28 14:59:57 +0700

    add new comment

--- src/modules/users/user.service.ts
+++ src/modules/users/user.service.ts
@@ -1,8 +1,10 @@
+import { PrismaService } from "../prisma/prisma.service";
 import { UserUpdateDTO } from "./dto/user-update.dto";
 
 export class userService {
+    private prisma: PrismaService;
     constructor() {
-
+        this.prisma = new PrismaService();
     }
 
     userUpdate = async (body:UserUpdateDTO) => {}
